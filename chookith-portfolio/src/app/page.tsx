15a59d0547c52ea0bc36ee14186b49469088bf21
Chookith <96812073+Chookith@users.noreply.github.com>
2024-01-21 19:14:21 +1000

    image fix i

--- chookith-portfolio/src/app/page.tsx
+++ chookith-portfolio/src/app/page.tsx
@@ -8,16 +8,16 @@ export default function Home() {
   return (
     <Suspense fallback={<Loading />}>
       <main>
-        <section className="overflow-y flex h-auto w-screen flex-col lg:h-screen lg:flex-row">
+        <section className="overflow-y flex h-auto w-screen flex-col xl:h-screen xl:flex-row">
           <section className="flex-col items-center justify-center sm:basis-1/2">
-            <h1 className="ml-10 mt-36 animate-dropIn items-center justify-center text-center text-5xl lg:ml-36">
+            <h1 className=" mt-36 animate-dropIn items-center justify-center text-center text-5xl xl:ml-36">
               {" "}
               I am
             </h1>
-            <h1 className="mt-7 animate-[dropIn2_1200ms_ease_400ms_backwards] items-center justify-center text-center text-7xl lg:ml-36">
+            <h1 className="mt-7 animate-[dropIn2_1200ms_ease_400ms_backwards] items-center justify-center text-center text-7xl xl:ml-36">
               Chookith
             </h1>
-            <div className="ml-10 flex animate-[dropIn_1200ms_ease_500ms_backwards] flex-row items-center justify-center py-7 lg:ml-36">
+            <div className=" flex animate-[dropIn_1200ms_ease_500ms_backwards] flex-row items-center justify-center py-7 xl:ml-36">
               <p className="mr-4 rounded-full bg-teagreen p-3 shadow-lg">
                 React
               </p>
@@ -28,28 +28,28 @@ export default function Home() {
                 TypeScript
               </p>
             </div>
-            <p className="my-5 ml-10 animate-dropIn2  text-center lg:ml-36">
+            <p className="animate-dropIn2 items-center justify-center text-center xl:ml-36">
               I'm a Front End Developer based in <br /> Queensland, Australia
             </p>
           </section>
-          <section className="flex items-center justify-center">
+          <section className="flex items-center justify-center xl:w-1/2">
             <Image
               src={myPhoto}
               alt="smiling woman with green head wrap, glasses, and a black shirt"
-              className="ml-10 mt-32 w-2/3 animate-[dropIn_1200ms_ease_700ms_backwards] rounded-3xl  lg:ml-36 lg:w-3/4 lg:basis-2/5"
+              className="mt-36 w-2/5 animate-[dropIn_1200ms_ease_700ms_backwards] rounded-3xl lg:w-4/5 lg:basis-1/2 xl:ml-36 xl:rounded-full"
             ></Image>
           </section>
         </section>
         <section className="h-screen w-screen flex-col ">
-          <h1 className="pb-10 pl-5 text-center text-5xl">About Me</h1>
+          <h1 className="mt-24 pb-10 pl-5 text-center text-5xl">About Me</h1>
           <div className="flex flex-row divide-x divide-solid">
-            <article className="mx-10 hidden flex-col items-center justify-center divide-x divide-solid lg:ml-36 lg:flex">
+            <article className="mx-10 hidden flex-col items-center justify-center lg:ml-36 lg:flex">
               <q className="rounded-full bg-ashblue p-16 text-4xl">
                 I am a mosaic of everyone I have ever loved even for a heartbeat
               </q>
-              <p>-viridianmasquerade</p>
+              <p> viridianmasquerade</p>
             </article>
-            <article className="mx-10 lg:ml-36">
+            <article className="mx-10 divide-x divide-solid lg:ml-36">
               <p className=" pl-5 text-center text-xl">
                 Hello I'm Charli also known as Chookith. I am a junior front-end
                 web developer who is based in Australia. After an accidental

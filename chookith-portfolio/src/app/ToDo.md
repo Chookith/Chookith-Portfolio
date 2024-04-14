1c9280d40357853042b8353ae2344e83fed0329b
Chookith <chookchook1@outlook.com>
2024-04-14 12:54:47 +1000

    Animation fix. Todo Added

--- chookith-portfolio/src/app/ToDo.md
+++ chookith-portfolio/src/app/ToDo.md
@@ -1,12 +1,79 @@
 # Broken Down
 
-## Pc [ x ]
+### General [ x ]
+
+- [ x ] PC small
+
+- [ x ] PC Large
+- [ x ] Tablet
+- [ x ] Phone
+- [ x ] Animations
+
+- [ x ] Colors
+
+  - [ x ] light
+
+    - midnightpurple: #413C58
+    - ashblue: #9BBAB2
+    - teagreen: #BFD7B5
+    - summergreen: #E7EFC5
+    - sunlightyellow: #F2DDA4
+    - cream: #FDF8ED
+
+  - [ ] Dark
+
+- [ x ] sound page design
+  - [ x ] even padding / margins
+  - [ x ] consistant color choice
+  - [ x ] consistant shadow for clickable and non-clickable items
+  - [ ] Ease of Navigation
+    - Unsure if header bar is necessary on 404 page.
+    - If you are getting a 404 chances are users are looking for it so...
+    - [ x ] Button to Get off 404
+    - [ x ] button to get home from **_redacted_**
 
 ### header [ x ]
 
 - [ x ] Links
-
   - [ x ] Homing
   - [ x ] Contact
 
--
+### Inital Page [ x ]
+
+- [ x ] Name
+- [ x ] Image
+- [ x ] Skills
+  - [ x ] Animation to draw attention
+
+### About Me
+
+- [ x ] Quote
+- [ x ] Who am I?
+- [ x ] Why did I get into this?
+- [ x ] Final goal
+- If we are doing front end dev Mention
+  - [ x ] Art
+  - [ x ] Design
+  - [ x ] Framework
+  - [ ] Accessability
+- If we are doing Back end mention:
+  - [ ] DataBase type
+  - [ ] Love of organisation?
+  - [ ] Logical Analysis
+  - [ ] Data Structure
+
+### Projects
+
+- [ x ] Themes
+- [ ] Pinterest Copy
+- [ ] Todo-ii
+- [ ] Back End project
+
+### Contact
+
+- [ x ] Git
+- [ x ] insta
+- [ x ] linkedin
+- [ x ] Email
+
+**_Make sure they are safe. I do not want to deal with karens clicking on bad links_**
--- chookith-portfolio/src/app/page.tsx
+++ chookith-portfolio/src/app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
           <h1 className="mb-10 animate-[dropIn_1200ms_ease_300ms_backwards] items-center justify-center text-center text-5xl">
             About Me
           </h1>
-          <div className="flex flex-col items-center justify-center divide-y lg:flex-row  lg:divide-x lg:divide-solid">
+          <div className="flex flex-col items-center justify-center divide-y lg:flex-row lg:divide-x  lg:divide-y-0 lg:divide-solid">
             <article className="animate-[dropIn_1200ms_ease_400ms_backwards] flex-col items-center justify-center lg:mr-5 lg:flex lg:basis-1/2 ">
               <q className="hidden rounded-full bg-ashblue p-16 text-2xl lg:flex xl:hidden">
                 i. first there was light and then there were colors, and from
@@ -91,7 +91,7 @@ export default function Home() {
                 <br /> I have looked but I couldn't find anything, so sorry.
               </p>
             </article>
-            <article className=" animate-[dropIn_1200ms_ease_500ms_backwards] lg:basis-1/2 lg:pl-5">
+            <article className="animate-[dropIn_1200ms_ease_500ms_backwards] lg:basis-1/2 lg:pl-5">
               <p className="pt-5 text-center text-xl lg:pt-0">
                 Hello I'm Charli also known as Chookith. I am a junior front-end
                 web developer who is based in Australia. After an accidental
@@ -115,8 +115,8 @@ export default function Home() {
               </p>
               <p className=" text-center text-xl">
                 <br />
-                Someday I plan to ease the strain of rural communities. More
-                specifically agricultural advancements
+                Someday I plan to ease the strain in rural communities. More
+                specifically in the agriculture industry.
               </p>
             </article>
           </div>
@@ -125,157 +125,144 @@ export default function Home() {
           <h1 className="mb-5 animate-[dropIn_1200ms_ease_300ms_backwards] text-center text-5xl">
             Projects
           </h1>
-          <ul className=" flex w-full flex-col items-center justify-center">
-            <h2 className="my-5 flex animate-[dropIn_1200ms_ease_400ms_backwards] items-center justify-center text-3xl">
-              Completed
-            </h2>
-            <section
-              id="themes"
-              className=" flex h-full w-full flex-col items-center justify-center lg:flex-row"
-            >
-              <li className="mt-5 flex w-full animate-[dropIn_1200ms_ease_500ms_backwards] flex-col items-center justify-center rounded-3xl bg-sunlightyellow p-5 shadow-md transition delay-75 hover:-translate-y-1 hover:shadow-xl lg:mx-5 lg:basis-1/3">
-                <Link
-                  href="https://marketplace.visualstudio.com/items?itemName=Chookith.coffee-theme-dark"
-                  target="_blank"
-                  rel="noreferrer noopener"
-                  className="w-full "
-                >
-                  <h1 className="text-2xl">Late Night Mocha Theme</h1>
-                  <ul className="flex flex-row flex-wrap items-center justify-center px-5 xl:flex-row">
-                    <li className="mr-2 mt-2 rounded-full bg-teagreen p-3 shadow-lg">
-                      Color Theory
-                    </li>
-                    <li className="mr-2 mt-2 rounded-full bg-teagreen p-3 shadow-lg">
-                      Vsce Api
-                    </li>
-                  </ul>
-                  <p className="mt-5 px-5 pb-2 text-center">
-                    Dark VSC Theme inspired by my one true love coffee.
-                  </p>
-                </Link>
-              </li>
-              <li className="mt-5 flex h-auto w-full animate-[dropIn_1200ms_ease_550ms_backwards] flex-col items-center justify-center rounded-3xl bg-sunlightyellow p-5 shadow-md transition delay-75 hover:-translate-y-1 hover:shadow-xl lg:mx-5  lg:basis-1/3">
-                <Link
-                  href="https://marketplace.visualstudio.com/items?itemName=Chookith.coffee-theme-light"
-                  target="_blank"
-                  rel="noreferrer noopener"
-                  className="w-full"
-                >
-                  <h1 className="text-2xl">Morning Coffee Theme</h1>
-                  <ul className="flex flex-row flex-wrap items-center justify-center px-5 xl:flex-row">
-                    <li className="mr-2 mt-2 rounded-full bg-teagreen p-3 shadow-lg">
-                      Color Theory
-                    </li>
-                    <li className="mr-2 mt-2 rounded-full bg-teagreen p-3 shadow-lg">
-                      Vsce Api
-                    </li>
-                  </ul>
-                  <p className="mt-5 px-5 pb-2 text-center">
-                    Light counter part of Late Night Mocha
-                  </p>
-                </Link>
-              </li>
-              <li className="mt-5 flex w-full animate-[dropIn_1200ms_ease_600ms_backwards] flex-col items-center justify-center rounded-3xl bg-sunlightyellow p-5 shadow-md transition delay-75 hover:-translate-y-1 hover:shadow-xl lg:mx-5 lg:basis-1/3">
-                <Link
-                  href="https://marketplace.visualstudio.com/items?itemName=Chookith.dark-fem-theme"
-                  target="_blank"
-                  rel="noreferrer noopener"
-                  className="w-full"
-                >
-                  <h1 className="text-2xl">Dark Fem Theme</h1>
-                  <ul className="flex flex-row flex-wrap items-center justify-center px-5 xl:flex-row">
-                    <li className="mr-2 mt-2 rounded-full bg-teagreen p-3 shadow-lg">
-                      Color Theory
-                    </li>
-                    <li className="mr-2 mt-2 rounded-full bg-teagreen p-3 shadow-lg">
-                      Vsce Api
-                    </li>
-                  </ul>
-                  <p className="mt-5 px-5 pb-2 text-center">
-                    Dark VSC Theme inspired by the greek goddess Aphrodite
-                  </p>
-                </Link>
-              </li>
-            </section>
-            <section
-              id="Line 2"
-              className=" mt-10 flex w-full flex-col items-center justify-center lg:flex-row"
-            >
-              <li className="flex w-full basis-1/3 animate-[dropIn_1200ms_ease_650ms_backwards] lg:mx-5"></li>
-              <li className=" flex  w-full animate-[dropIn_1200ms_ease_700ms_backwards] flex-col items-center justify-center rounded-3xl bg-cream p-5 shadow-md transition delay-75 hover:-translate-y-1 hover:shadow-xl lg:mx-5 lg:w-auto lg:basis-1/3">
-                <h1 className="text-2xl">Portfolio</h1>
-                <ul className="flex  flex-wrap items-center justify-center xl:flex-row">
+          <h2 className="my-5 flex animate-[dropIn_1200ms_ease_400ms_backwards] items-center justify-center text-3xl">
+            Completed
+          </h2>
+          <ul className="row-auto grid w-full grid-cols-1 flex-col items-center justify-center gap-5 lg:grid-cols-3">
+            <li className="flex w-full animate-[dropIn_1200ms_ease_500ms_backwards] flex-col items-center justify-center rounded-3xl bg-sunlightyellow p-5 shadow-md transition delay-75 hover:-translate-y-1 hover:shadow-xl">
+              <Link
+                href="https://marketplace.visualstudio.com/items?itemName=Chookith.coffee-theme-dark"
+                target="_blank"
+                rel="noreferrer noopener"
+                className="w-full "
+              >
+                <h1 className="text-2xl">Late Night Mocha Theme</h1>
+                <ul className="flex flex-row flex-wrap items-center justify-center px-5 xl:flex-row">
                   <li className="mr-2 mt-2 rounded-full bg-teagreen p-3 shadow-lg">
-                    React
+                    Color Theory
                   </li>
                   <li className="mr-2 mt-2 rounded-full bg-teagreen p-3 shadow-lg">
-                    NextJS
-                  </li>
-                  <li className="mr-2 mt-2 rounded-full bg-teagreen p-3 shadow-lg">
-                    CSS
-                  </li>
-                  <li className="mr-2 mt-2 rounded-full bg-teagreen p-3 shadow-lg">
-                    NodeJS
-                  </li>
-                  <li className="mr-2 mt-2 rounded-full bg-teagreen p-3 shadow-lg">
-                    TypeScript
-                  </li>
-                  <li className="mr-2 mt-2 rounded-full bg-teagreen p-3 shadow-lg">
-                    TailwindCSS
+                    Vsce Api
                   </li>
                 </ul>
                 <p className="mt-5 px-5 pb-2 text-center">
-                  With the skills I've been refining, over the last 4 years,
-                  I've managed to come up with this.
+                  Dark VSC Theme inspired by my one true love coffee.
                 </p>
-              </li>
-              <li className="flex w-full basis-1/3 animate-[dropIn_1200ms_ease_750ms_backwards] lg:mx-5"></li>
-            </section>
-            <h2 className="my-10 flex animate-[dropIn_1200ms_ease_800ms_backwards] items-center justify-center text-3xl">
-              In Progress
-            </h2>
-            <section
-              id="Line 2"
-              className=" flex w-full flex-col items-center justify-center lg:flex-row"
-            >
-              <li className="flex w-full animate-[dropIn_1200ms_ease_850ms_backwards] flex-col items-center justify-center rounded-3xl bg-summergreen p-5 shadow-md transition delay-75 hover:-translate-y-1 hover:shadow-xl lg:mx-5 lg:w-auto lg:basis-1/3">
-                <h1 className="text-2xl">Todo-ii App</h1>
-                <ul className="flex  flex-wrap items-center justify-center xl:flex-row">
+              </Link>
+            </li>
+            <li className="flex w-full animate-[dropIn_1200ms_ease_550ms_backwards] items-center justify-center rounded-3xl bg-sunlightyellow p-5 shadow-md transition delay-75 hover:-translate-y-1 hover:shadow-xl">
+              <Link
+                href="https://marketplace.visualstudio.com/items?itemName=Chookith.coffee-theme-light"
+                target="_blank"
+                rel="noreferrer noopener"
+                className="w-full"
+              >
+                <h1 className="text-2xl">Morning Coffee Theme</h1>
+                <ul className="flex flex-row flex-wrap items-center justify-center px-5 xl:flex-row">
                   <li className="mr-2 mt-2 rounded-full bg-teagreen p-3 shadow-lg">
-                    React
+                    Color Theory
                   </li>
                   <li className="mr-2 mt-2 rounded-full bg-teagreen p-3 shadow-lg">
-                    TypeScript
-                  </li>
-                  <li className="mr-2 mt-2 rounded-full bg-teagreen p-3 shadow-lg">
-                    TailwindCSS
+                    Vsce Api
                   </li>
                 </ul>
-              </li>
-              <li className="mt-10 flex w-full animate-[dropIn_1200ms_ease_900ms_backwards] flex-col items-center justify-center rounded-3xl bg-summergreen p-5 shadow-md transition delay-75 hover:-translate-y-1 hover:shadow-xl md:mt-10 lg:mx-5 lg:mt-0 lg:w-auto lg:basis-1/3">
-                {" "}
-                <h1 className="text-2xl">Pinterest Remake</h1>
-                <ul className="flex  flex-wrap items-center justify-center xl:flex-row">
+                <p className="mt-5 px-5 pb-2 text-center">
+                  Light counter part of Late Night Mocha
+                </p>
+              </Link>
+            </li>
+            <li className="flex  w-full animate-[dropIn_1200ms_ease_600ms_backwards]  items-center justify-center rounded-3xl bg-sunlightyellow p-5 shadow-md transition delay-75 hover:-translate-y-1 hover:shadow-xl">
+              <Link
+                href="https://marketplace.visualstudio.com/items?itemName=Chookith.dark-fem-theme"
+                target="_blank"
+                rel="noreferrer noopener"
+                className="w-full"
+              >
+                <h1 className="text-2xl">Dark Fem Theme</h1>
+                <ul className="flex flex-row flex-wrap items-center justify-center px-5 xl:flex-row">
                   <li className="mr-2 mt-2 rounded-full bg-teagreen p-3 shadow-lg">
-                    React
+                    Color Theory
                   </li>
                   <li className="mr-2 mt-2 rounded-full bg-teagreen p-3 shadow-lg">
-                    TypeScript
-                  </li>
-                  <li className="mr-2 mt-2 rounded-full bg-teagreen p-3 shadow-lg">
-                    TailwindCSS
+                    Vsce Api
                   </li>
                 </ul>
-              </li>
-            </section>
+                <p className="mt-5 px-5 pb-2 text-center">
+                  Dark VSC Theme inspired by the greek goddess Aphrodite
+                </p>
+              </Link>
+            </li>
+            <li className="flex w-full animate-[dropIn_1200ms_ease_650ms_backwards] "></li>
+            <li className=" flex  w-full animate-[dropIn_1200ms_ease_700ms_backwards] flex-col items-center justify-center rounded-3xl bg-cream p-5 shadow-md transition delay-75 hover:-translate-y-1 hover:shadow-xl ">
+              <h1 className="text-2xl">Portfolio</h1>
+              <ul className="flex  flex-wrap items-center justify-center xl:flex-row">
+                <li className="mr-2 mt-2 rounded-full bg-teagreen p-3 shadow-lg">
+                  React
+                </li>
+                <li className="mr-2 mt-2 rounded-full bg-teagreen p-3 shadow-lg">
+                  NextJS
+                </li>
+                <li className="mr-2 mt-2 rounded-full bg-teagreen p-3 shadow-lg">
+                  CSS
+                </li>
+                <li className="mr-2 mt-2 rounded-full bg-teagreen p-3 shadow-lg">
+                  NodeJS
+                </li>
+                <li className="mr-2 mt-2 rounded-full bg-teagreen p-3 shadow-lg">
+                  TypeScript
+                </li>
+                <li className="mr-2 mt-2 rounded-full bg-teagreen p-3 shadow-lg">
+                  TailwindCSS
+                </li>
+              </ul>
+              <p className="mt-5 px-5 pb-2 text-center">
+                With the skills I've been refining, over the last 4 years, I've
+                managed to come up with this.
+              </p>
+            </li>
+            <li className="flex w-full  animate-[dropIn_1200ms_ease_750ms_backwards]"></li>
+          </ul>
+          <h2 className="my-10 flex animate-[dropIn_1200ms_ease_800ms_backwards] items-center justify-center text-3xl">
+            In Progress
+          </h2>
+          <ul className="row-auto grid w-full grid-cols-1 flex-col items-center justify-center gap-5 lg:grid-cols-3">
+            <li className="flex w-full animate-[dropIn_1200ms_ease_850ms_backwards] flex-col items-center justify-center rounded-3xl bg-summergreen p-5 shadow-md transition delay-75 hover:-translate-y-1 hover:shadow-xl">
+              <h1 className="text-2xl">Todo-ii App</h1>
+              <ul className="flex  flex-wrap items-center justify-center xl:flex-row">
+                <li className="mr-2 mt-2 rounded-full bg-teagreen p-3 shadow-lg">
+                  React
+                </li>
+                <li className="mr-2 mt-2 rounded-full bg-teagreen p-3 shadow-lg">
+                  TypeScript
+                </li>
+                <li className="mr-2 mt-2 rounded-full bg-teagreen p-3 shadow-lg">
+                  TailwindCSS
+                </li>
+              </ul>
+            </li>
+            <li className=" 3 flex w-full animate-[dropIn_1200ms_ease_900ms_backwards] flex-col items-center justify-center rounded-3xl bg-summergreen p-5 shadow-md transition delay-75 hover:-translate-y-1 hover:shadow-xl">
+              {" "}
+              <h1 className="text-2xl">Pinterest Remake</h1>
+              <ul className="flex  flex-wrap items-center justify-center xl:flex-row">
+                <li className="mr-2 mt-2 rounded-full bg-teagreen p-3 shadow-lg">
+                  React
+                </li>
+                <li className="mr-2 mt-2 rounded-full bg-teagreen p-3 shadow-lg">
+                  TypeScript
+                </li>
+                <li className="mr-2 mt-2 rounded-full bg-teagreen p-3 shadow-lg">
+                  TailwindCSS
+                </li>
+              </ul>
+            </li>
           </ul>
         </section>
-        <section className=" h-full w-full  rounded-2xl bg-ashblue px-10 py-10 shadow-xl">
+        <section className=" h-full  w-full rounded-2xl bg-ashblue px-10 py-10 shadow-xl">
           <h1 className="mt-5 animate-[dropIn_1200ms_ease_300ms_backwards] pb-10 text-center text-5xl lg:mt-5 lg:pl-5">
             Let's Get In Contact
           </h1>
           <article className=" flex w-full basis-1/2 flex-col lg:flex-row lg:divide-x lg:divide-y-0">
-            <div className="mr-5 grid basis-1/4 grid-cols-2 grid-rows-2 items-center justify-center gap-2 xl:mr-5 ">
+            <div className="mb-5 grid basis-1/4 grid-cols-2 grid-rows-2 items-center justify-center gap-2 md:mr-5 lg:mb-0 ">
               <Link
                 href="mailto:chookchook1@outlook.com"
                 className="m-1 flex animate-[dropIn_1200ms_ease_350ms_backwards] items-center rounded-full bg-cream p-5 text-center shadow-md transition delay-75 hover:-translate-y-1 hover:shadow-xl"
